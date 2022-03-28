a0bb772fd5b71caa6fe33edd76fde9c72f0574ee
Printsevskaya <helloman005@gmail.com>
2022-03-28 19:40:56 +0300

    Commit2

--- src/HomeWork1/WithoutDivMod.java
+++ src/HomeWork1/WithoutDivMod.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class WithoutDivMod {
     public static void delenie() {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt();
         int y = scanner.nextInt();
         int div = 0;
 
-        while(x >=y){
-        x -= y;
-        div++;
-    }
+        while (x >= y) {
+            x -= y;
+            div++;
+        }
         System.out.print(div + " " + x);
-}
+    }
 
-public static void main(String[] args) {
-    delenie();
-}
+    public static void main(String[] args) {
+        delenie();
+    }
 
 }
